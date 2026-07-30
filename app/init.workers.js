cd10587787b0ae815ca252820865085ce27b386d
Satar Sarafa <sp3c7r40x00@gmail.com>
2026-07-30 06:47:30 +0100

    fix: worker process wasn't connecting to MongoDB — payments buffered and timed out

--- app/init.workers.js
+++ app/init.workers.js
@@ -1,5 +1,10 @@
+import { mongoConnection } from "../src/connection/mongo.connection.js";
+import { redisConnection } from "../src/connection/redis.connection.js";
 import { EmailWorkerService } from "../src/services/workers/email.worker.service.js";
 import { PaymentWorkerService } from "../src/services/workers/payment.worker.service.js";
 
-EmailWorkerService.getInstance();
-PaymentWorkerService.getInstance();
+redisConnection();
+mongoConnection(() => {
+	EmailWorkerService.getInstance();
+	PaymentWorkerService.getInstance();
+});
